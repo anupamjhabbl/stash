a4ee2163c521ea549d74f6486f9dccb0dd76633a
anupamjhabbl <anupamimpbbl@gmail.com>
2025-12-19 00:22:27 +0530

    Initialized koin for Desktop.

--- composeApp/src/jvmMain/kotlin/com/example/stash/main.kt
+++ composeApp/src/jvmMain/kotlin/com/example/stash/main.kt
@@ -2,12 +2,31 @@ package com.example.stash
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import com.example.stash.di.commonModule
+import com.example.stash.di.databaseModule
+import com.example.stash.di.viewModelModule
 
-fun main() = application {
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = "Stash",
-    ) {
-        App()
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+
+fun main() {
+
+    startKoin {
+        modules(
+            databaseModule,
+            commonModule,
+            viewModelModule
+        )
+    }
+    application {
+        Window(
+            onCloseRequest = {
+                stopKoin()
+                exitApplication()
+            },
+            title = "Stash"
+        ) {
+            App()
+        }
     }
 }
